8d7ba74c46cd61aa7d50680f475fa9950209d317
ElisaFiorenza <119570190+ElisaFiorenza@users.noreply.github.com>
2024-05-28 08:54:30 +0200

    sfondo finito
    sfondo dinamico fatto

--- mainscene/node_2d.gd
+++ mainscene/node_2d.gd
@@ -1,28 +1,34 @@
 extends Node2D
 @onready var pig = $Pig
-const KEBABBONI = preload("res://kebabboni.tscn")
-var kebap_position = 10
-var arraykebap = [0,0]
+@onready var parallax_background = $ParallaxBackground
+
+
+const FLAMES = preload("res://kebabboni.tscn")
+var scroll_speed = Vector2(0, 10) # Velocità di scorrimento (X, Y)
+var flame_position = 10
+var arrayflame = [0,0]
 var i
 func _ready():
-	var first_kebap = KEBABBONI.instantiate()
-	arraykebap[0] = first_kebap;
+	var first_flame = FLAMES.instantiate()
+	arrayflame[0] = first_flame;
 	await get_tree().create_timer(2.0).timeout
-	first_kebap.position.x = kebap_position
-	first_kebap.position.y = randf_range(-100,100)
-	kebap_position += 360
+	first_flame.position.x = flame_position
+	first_flame.position.y = randf_range(-100,100)
+	flame_position += 360
 	while true:
-		var new_kebap = KEBABBONI.instantiate()
-		add_child(new_kebap)
-		new_kebap.position.x = kebap_position
-		new_kebap.position.y = randf_range(-100,100)
-		kebap_position += 360
-		var last_kebap = arraykebap[1]
-		arraykebap[1] = first_kebap
-		arraykebap[0] = new_kebap
-		first_kebap = new_kebap
-		if kebap_position > 1080: 
-			last_kebap.queue_free() 
+		var new_flame = FLAMES.instantiate()
+		add_child(new_flame)
+		new_flame.position.x = flame_position
+		new_flame.position.y = randf_range(-100,100)
+		flame_position += 360
+		var last_flame = arrayflame[1]
+		arrayflame[1] = first_flame
+		arrayflame[0] = new_flame
+		first_flame = new_flame
+		if flame_position > 1080: 
+			last_flame.queue_free() 
 		await get_tree().create_timer(4.2).timeout
-		
+func _process(delta):
+	parallax_background.scroll_offset += scroll_speed * delta
+
 
