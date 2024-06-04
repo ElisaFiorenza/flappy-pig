c87ddf0ba641df7af3a0763b066539a9ea4a399e
ElisaFiorenza <119570190+ElisaFiorenza@users.noreply.github.com>
2024-06-04 20:01:20 +0200

    completata versione provvisoria
    yeee

--- mainscene/node_2d.gd
+++ mainscene/node_2d.gd
@@ -2,21 +2,22 @@ extends Node2D
 @onready var pig = $Pig
 @onready var parallax_background = $ParallaxBackground
 @onready var points = $Camera2D/points
-@onready var point_audio = $"point-audio"
+@onready var point_audio = $"Pig/point-audio"
 
 
 const FLAMES = preload("res://kebabboni.tscn")
 var scroll_speed = Vector2(0, 10) # Velocità di scorrimento (X, Y)
-var flame_position = 10
+var flame_position = 320
 var arrayflame = [0,0]
 func _ready():
 	var first_flame = FLAMES.instantiate()
 	arrayflame[0] = first_flame;
-	await get_tree().create_timer(2.0).timeout
 	first_flame.position.x = flame_position
-	first_flame.position.y = randf_range(-100,100)
+	first_flame.position.y = randf_range(-110,110)
 	flame_position += 360
 	points.text = "0" 
+	add_child(first_flame)
+	await get_tree().create_timer(6).timeout
 	
 	while true:
 		var new_flame = FLAMES.instantiate()
@@ -30,7 +31,7 @@ func _ready():
 		first_flame = new_flame
 		if flame_position > 1080: 
 			last_flame.queue_free() 
-		await get_tree().create_timer(4.2).timeout
+		await get_tree().create_timer(4).timeout
 func _process(delta):
 	parallax_background.scroll_offset += scroll_speed * delta
 
